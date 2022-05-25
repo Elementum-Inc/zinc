d69a0522c3bed8402555ebdc7c9d497753b71049
Dani Reinking <dani@elementumtech.io>
2022-05-25 13:01:26 -0400

    Main script updated with WindiCSS.

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,7 +1,8 @@
 import "vite/modulepreload-polyfill";
 import { createApp } from "vue";
-import App from "../vue/App.vue";
+import App from "./vue/App.vue";
 
+import "virtual:windi.css";
 import "../styles/theme.css";
 
 createApp(App).mount("#app");
