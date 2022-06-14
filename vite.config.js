7a6fb18eefc9e4d1a372173f8e885dd39ed894dd
Dani Reinking <dani@elementumtech.io>
2022-06-14 10:19:35 -0400

    Npm scripts and vite config split for dev and prod.

--- vite.config.js
+++ vite.config.js
@@ -2,29 +2,52 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
 
-export default defineConfig({
-  mode: 'development', // [TODO] Remove or change mode before submission, want to make sure merchants are using it in prod out of the box 👍
-  plugins: [vue(), WindiCSS()],
-  publicDir: false,
-  build: {
-    rollupOptions: {
-      input: './scripts/theme.js',
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    },
-    outDir: 'assets',
-    assetsDir: './',
-    emptyOutDir: false,
-    watch: {
-      include: [
-        './scripts/theme.js',
-        './scripts/**/*.js',
-        './styles/theme.css',
-        './styles/**/*.css',
-      ]
-    },
-  },
+export default defineConfig(({ command, mode }) => {
+  switch (mode) {
+    case 'development':
+      return {
+        plugins: [vue(), WindiCSS()],
+        publicDir: false,
+        build: {
+          rollupOptions: {
+            input: './scripts/theme.js',
+            output: {
+              entryFileNames: '[name].js',
+              chunkFileNames: '[name].js',
+              assetFileNames: '[name].[ext]'
+            }
+          },
+          outDir: 'assets',
+          assetsDir: './',
+          emptyOutDir: false,
+          watch: {
+            include: [
+              './scripts/theme.js',
+              './scripts/**/*.js',
+              './styles/theme.css',
+              './styles/**/*.css',
+            ]
+          },
+        },
+      };
+    case 'production':
+      return {
+        plugins: [vue(), WindiCSS()],
+        publicDir: false,
+        build: {
+          rollupOptions: {
+            input: './scripts/theme.js',
+            output: {
+              entryFileNames: '[name].js',
+              chunkFileNames: '[name].js',
+              assetFileNames: '[name].[ext]'
+            }
+          },
+          outDir: 'assets',
+          assetsDir: './',
+          emptyOutDir: false,
+          watch: null
+        },
+      };
+  }
 });
