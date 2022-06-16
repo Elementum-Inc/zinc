a030f5ccb6c2b31ed1e7483e847d2cfeda013813
Dani Reinking <dani@elementumtech.io>
2022-06-16 12:59:26 -0400

    Fixing path according to docs.

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,5 @@ module.exports = {
     'no-undef': 'off',
     'no-unused-vars': 'warn'
   },
-  ignorePatterns: ['assets/theme.js', 'cypress/*', '*.js']
+  ignorePatterns: ['assets/theme.js', 'cypress/*', '/*.js']
 };
