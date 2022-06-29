a5e475d497e959663f6211829e975751f55729ca
Dani Reinking <dani@elementumtech.io>
2022-06-29 16:56:01 -0400

    Minor cleanup in vue boilerplate.

--- vue/App.vue
+++ vue/App.vue
@@ -7,13 +7,3 @@ import HelloWorld from './components/HelloWorld.vue';
 <template>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  margin-top: 60px;
-}
-</style>
