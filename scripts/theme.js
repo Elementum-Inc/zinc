60c2c62bc9e1d2a2537d1d1eac34937c99d6b303
Dani Reinking <dani@elementumtech.io>
2022-07-14 16:41:50 -0400

    🔥 hf: Just a slight reorder in main js.

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,10 +1,9 @@
-import 'vite/modulepreload-polyfill';
-
 import 'virtual:windi.css';
 import '../styles/base/typography.css';
 import '../styles/base/buttons.css';
 import '../styles/theme.css';
 
+import 'vite/modulepreload-polyfill';
 import { createApp } from 'vue';
 import App from '../vue/App.vue';
 
