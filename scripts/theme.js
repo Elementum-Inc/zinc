0971043b084cfb9d9876d6423011594fa4e5624e
Dani Reinking <dani@elementumtech.io>
2022-07-18 08:57:10 -0400

    Fixing some duplication in theme js.

--- scripts/theme.js
+++ scripts/theme.js
@@ -6,11 +6,6 @@ import '../styles/theme.css';
 
 import 'vite/modulepreload-polyfill';
 
-import 'virtual:windi.css';
-import '../styles/base/typography.css';
-import '../styles/base/buttons.css';
-import '../styles/theme.css';
-
 import { createApp } from 'vue';
 import App from '../vue/App.vue';
 
