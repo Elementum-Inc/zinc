f237dfc79eab5a7c3b0eb391439207b3fb44b8cc
Dani Reinking <5230487+danipeach@users.noreply.github.com>
2022-08-31 14:37:59 -0400

    👒 Header - Megamenu 🪗 (#13)
    * LOTS of changes, but mostly: header building, adding icons, related schema and settings.
    
    * Compiled theme files.
    
    * Undo change from tailwind experiment.
    
    * Assets compiled.
    
    * Search modal content coming along, results populating.
    
    * Compiled theme files.
    
    * Couple tiny additions in Windi/StyleLint configs.
    
    * New settings schema/data for cards and search.
    
    * New styles for cards and general animations, moved search into header sheet.
    
    * New component for basic image tags, search component 95%.
    
    * Package updates, compiled assets.
    
    * Compiled theme files.
    
    * Fetched changes, reduced specifcation on cypress action.
    
    * Compiled theme files.
    
    * Some final accessibility updates to cover Shopify reqs.
    
    * Compiled assets.
    
    * Compiled assets.
    
    * Megamenu blocks, settings, and styles. Some addt. general styling.
    
    * Compiled assets.
    
    * Compiled theme files.
    
    * Removed unused fetch method, recompiled.
    
    * Removed absolute positioning on submenu bold, other minor fixes.
    
    * Compiled theme files.
    
    * Attempting to increase lh accessbility score with button names.
    
    * Compiled theme files.
    
    * Theme check fixes.
    
    * Theme check ignore.
    
    * Changing severity of unused rules.
    
    * Added aria-labels to icon buttons.
    
    * Compiled theme files.
    
    * Using the right syntax now for theme check. 😅
    
    Co-authored-by: danipeach <danipeach@users.noreply.github.com>

--- scripts/theme.js
+++ scripts/theme.js
@@ -10,7 +10,29 @@ import '../styles/snippets/cards.css';
 import 'vite/modulepreload-polyfill';
 
 import { createApp } from 'vue';
+import MegaMenu from '../vue/MegaMenu.vue';
 import SearchMenu from '../vue/SearchMenu.vue';
+
+const searchMount = document.querySelector('#searchMenuTop');
+const megamenuMount = document.querySelector('#megamenu');
+
+var megamenuSettings = JSON.parse(megamenuMount.dataset.settings);
+var megamenuBlocks = JSON.parse(megamenuMount.dataset.blocks);
+var topMenu = JSON.parse(megamenuMount.dataset.topmenu);
+var mobileLinks = JSON.parse(megamenuMount.dataset.mobilelinks);
+
+topMenu.forEach((m) => m.blocks = []);
+
+const menuProps = {
+  iconSize: window.themeSettings.icon_size,
+  iconStrokeWidth: window.themeSettings.icon_stroke_width,
+  settings: megamenuSettings,
+  blocks: megamenuBlocks,
+  topMenu: topMenu,
+  mobileLinks: mobileLinks
+};
+
+var searchSettings = JSON.parse(searchMount.dataset.settings);
 
 const searchProps = {
   searchPosition: window.themeSettings.search_open_position,
@@ -30,7 +52,32 @@ const searchProps = {
   cardImageFit: window.themeSettings.card_image_fit,
   cardAnimate: window.themeSettings.card_hover_animate,
   cardAnimation: window.themeSettings.card_hover_animation,
-  cardShowInfoOnHover: window.themeSettings.card_hover_show_info
+  cardShowInfoOnHover: window.themeSettings.card_hover_show_info,
+  settings: searchSettings
 };
 
-createApp(SearchMenu, searchProps).mount('#searchMenuTop');
+createApp(MegaMenu, menuProps).mount(megamenuMount);
+createApp(SearchMenu, searchProps).mount(searchMount);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('#shopify-section-header');
+  var ticking = false;
+
+  document.addEventListener('scroll', () => {
+    var yPos = window.scrollY;
+  
+    if (!ticking) {
+      window.requestAnimationFrame(() => {
+        if (yPos < window.innerHeight) {
+          header.classList.remove('sticky');
+        } else {
+          header.classList.add('sticky');
+        }
+
+        ticking = false;
+      });
+  
+      ticking = true;
+    }
+  });
+});
